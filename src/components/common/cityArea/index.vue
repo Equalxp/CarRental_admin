cbae6d24f47684153c838faa69132836dc5f3069
equalxp <2106704492@qq.com>
2023-09-13 21:07:22 +0800

    feat: 省市区组件优化

--- src/components/common/cityArea/index.vue
+++ src/components/common/cityArea/index.vue
@@ -9,61 +9,56 @@ import { GetCity } from "@/api/common"
 export default {
   name: "",
   data() {
+    const _this = this
     return {
+      address: [],
+      addressData: {},
       value: "",
       cascader_options: [],
       cascader_props: {
-        // 动态加载
         lazy: true,
-        // node当前点击的节点 resolve为数据加载完成的回调
         lazyLoad(node, resolve) {
           console.log("node", node)
-          // 初始的时候就是0 点击选择之后1
+          // 一开始就执行 node为0
           const level = node.level
           // 请求参数
           const requestData = {}
-          // 省份
-          if (level === 0) {
-            // 获取省请求参数只有用type
-            requestData.type = "province"
+          // 声明自定义配置
+          const jsonType = {
+            0: { type: "province" },
+            1: { type: "city", code: "province" },
+            2: { type: "area", code: "city" }
           }
-          // 城市
-          if (level === 1) {
-            requestData.type = "city"
-            requestData.province_code = node.value
+          // 有code的情况 拿到code
+          if (jsonType[level].code) {
+            requestData[`${jsonType[level].code}_code`] = node.value
           }
-          // 区/县
-          if (level === 2) {
-            requestData.type = "area"
-            requestData.city_code = node.value
-          }
+          // type 拿到对应的type
+          requestData.type = jsonType[level].type
           // 省市区的接口
           GetCity(requestData).then(resonse => {
             const data = resonse.data.data
-            
-            console.log("getCity", data)
-            if (level === 0) {
-              data.forEach(item => {
-                item.value = item.PROVINCE_CODE
-                item.label = item.PROVINCE_NAME
-              })
-            }
-            if (level === 1) {
-              data.forEach(item => {
-                item.value = item.CITY_CODE
-                item.label = item.CITY_NAME
-              })
-            }
-            if (level === 2) {
-              data.forEach(item => {
-                item.value = item.AREA_CODE
-                item.label = item.AREA_NAME
+            // 类型 变大写(参数)
+            const type = jsonType[level].type.toUpperCase()
+            // 自定义value、label
+            data.forEach(item => {
+              // value/lable用于级联的显示
+              item.value = item[`${type}_CODE`]
+              item.label = item[`${type}_NAME`]
+              // 最后一层选择
+              if (level === 2) {
                 item.leaf = level >= 2
-              })
-            }
+              }
+            })
+            // 存储省市区数据
+            _this.addressData[jsonType[level].type] = data
             // 返回节点数据
             resolve(data)
           })
+          // 获取address
+          if (node.level !== 0) {
+            _this.getAddress(node)
+          }
         }
       }
     }
@@ -71,6 +66,27 @@ export default {
   methods: {
     changeValue(value) {
       this.$emit("update:cityAreaValue", value.join())
+      // 匹配最后一项，区县
+      const lastCode = value[value.length - 1]
+      const area = this.addressData.area.filter(item => item.value == lastCode)[0]
+      this.address[2] = area.label
+      this.getAddress()
+    },
+    /** 获取中文地址 */
+    getAddress(node) {
+      if (node) {
+        const index = node.level - 1
+        this.address[index] = node.label
+      }
+      // 为 true 时，执行地图交互
+      if (this.mapLocation) {
+        this.$emit("callback", {
+          function: "setMapCenter",
+          data: {
+            address: this.address.join("")
+          }
+        })
+      }
     }
   },
   components: {},
@@ -78,6 +94,10 @@ export default {
     cityAreaValue: {
       type: String,
       default: ""
+    },
+    mapLocation: {
+      type: Boolean,
+      default: false
     }
   }
 }
